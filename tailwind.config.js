45885b47543fb4d338eaf39ee109b781c935ed84
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-04-17 23:38:58 +0300

    feat: update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
+	darkMode: 'class',
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				purpleLight: '#3450A1',
+				purpleDark: '#041955',
+				purpleBright: '#DF76FE',
+			},
+		},
 	},
 	plugins: [],
 }
