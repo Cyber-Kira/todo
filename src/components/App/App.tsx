e9263bfbdc2cd9ba4f47be9432103a2cd618c3a5
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-04-18 18:22:29 +0300

    feat: update configs

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { toggleThemeClass } from '../../utils'
+import { Background } from '../Background'
 import { ThemeToggle } from '../ThemeToggle'
+import { TotoItem } from '../TodoItem'
+
+// Running this function on first render
+// to remove transition between default
+// dark theme to light theme if latest
+// was selected by user
+toggleThemeClass()
 
 export const App = () => (
-	<>
-		<div className='text-3xl dark:text-slate-50 text-center mt-6 font-bold font-mono transition-colors'>
+	<div className='max-w-md mx-auto'>
+		<div className='text-3xl text-lightTextColor dark:text-slate-50 text-center mt-6 font-bold font-mono transition-colors'>
 			#todo
 		</div>
+		<div className='mt-10 px-8 flex flex-col gap-1'>
+			<TotoItem title='Daily meeting with team' />
+			<TotoItem title='Pay for rent' />
+			<TotoItem title='Check emails' />
+			<TotoItem title='Lunch with Emma' />
+			<TotoItem title='Meditation' />
+		</div>
 		<ThemeToggle />
-	</>
+		<Background />
+	</div>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,38 @@ module.exports = {
 				purpleLight: '#3450A1',
 				purpleDark: '#041955',
 				purpleBright: '#DF76FE',
+				light: '#fffeff',
+				lightDimmed: '#faf9ff',
+				lightTextColor: '#262755',
+				normalPriority: '#1857C0',
+			},
+			keyframes: {
+				wiggle: {
+					'0%': {
+						transform: 'translate(0px)',
+					},
+					'20%': {
+						transform: 'translate(5px)',
+					},
+					'40%': {
+						transform: 'translate(0px)',
+					},
+					'70%': {
+						color: 'inherit',
+					},
+					'100%': {
+						color: 'rgb(100 116 139)',
+					},
+				},
+				dimmed: {
+					to: {
+						backgroundColor: 'rgb(100 116 139)',
+					},
+				},
+			},
+			animation: {
+				wiggle: 'wiggle 1s ease-in forwards',
+				dimmed: 'dimmed 1s ease-in forwards',
 			},
 		},
 	},
