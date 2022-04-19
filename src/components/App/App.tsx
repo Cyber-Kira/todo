69345b1ea8c5b4e6a59cdde441ec8577fdca21fc
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-04-19 19:30:42 +0300

    WIP: add menu toggle with animations

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { toggleThemeClass } from '../../utils'
 import { Background } from '../Background'
 import { ThemeToggle } from '../ThemeToggle'
@@ -10,19 +10,44 @@ import { TotoItem } from '../TodoItem'
 // was selected by user
 toggleThemeClass()
 
-export const App = () => (
-	<div className='max-w-md mx-auto'>
-		<div className='text-3xl text-lightTextColor dark:text-slate-50 text-center mt-6 font-bold font-mono transition-colors'>
-			#todo
-		</div>
-		<div className='mt-10 px-8 flex flex-col gap-1'>
-			<TotoItem title='Daily meeting with team' />
-			<TotoItem title='Pay for rent' />
-			<TotoItem title='Check emails' />
-			<TotoItem title='Lunch with Emma' />
-			<TotoItem title='Meditation' />
-		</div>
-		<ThemeToggle />
-		<Background />
-	</div>
-)
+export const App = () => {
+	const [open, setOpen] = useState(false)
+	const toggleMenu = () => {
+		setOpen(!open)
+	}
+
+	return (
+		<>
+			<div
+				className={`fixed top-0 left-0 w-full h-full ${
+					open ? 'animate-slideIn pointer-events-none' : 'animate-slideOut'
+				} max-w-md mx-auto`}
+			>
+				<div className='text-3xl text-lightTextColor dark:text-slate-50 text-center mt-6 font-bold font-mono transition-colors'>
+					#todo
+				</div>
+				<button
+					onClick={toggleMenu}
+					type='button'
+					className='bg-white rounded-md ml-2 p-2 pointer-events-auto'
+				>
+					menu
+				</button>
+				<div className='mt-10 px-8 flex flex-col gap-1'>
+					<TotoItem title='Daily meeting with team' />
+					<TotoItem title='Pay for rent' />
+					<TotoItem title='Check emails' />
+					<TotoItem title='Lunch with Emma' />
+					<TotoItem title='Meditation' />
+				</div>
+				<ThemeToggle />
+				<Background
+					colorLight='bg-lightDimmed'
+					colorDark='dark:bg-purpleLight'
+					rounded={open}
+				/>
+			</div>
+			<Background colorLight='bg-purpleDark' colorDark='bg-purpleDark' />
+		</>
+	)
+}
