12fd5e3a1c0dd12ae5968e188792430996009c7e
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-04-19 19:30:14 +0300

    feat: add props for custom background

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
 
-export const Background = () => (
-	<div className='fixed top-0 left-0 bg-lightDimmed dark:bg-purpleLight transition-colors w-screen h-screen -z-10' />
+export const Background = ({
+	colorLight,
+	colorDark,
+	rounded,
+}: {
+	colorLight: string
+	colorDark: string
+	rounded?: boolean
+}) => (
+	<div
+		className={`fixed top-0 left-0 ${colorLight} ${colorDark} w-screen h-screen -z-10 ${
+			rounded ? 'animate-roundIn' : 'animate-roundOut'
+		}`}
+	/>
 )
