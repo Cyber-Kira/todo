c362d0e8e7fc1881a43837f386a98c515d9de4c1
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-04-22 21:12:13 +0300

    refactor: make background lower

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -3,11 +3,9 @@ import React from 'react'
 export const Background = ({
 	colorLight,
 	colorDark,
-	rounded,
 }: {
 	colorLight: string
 	colorDark: string
-	rounded?: boolean
 }) => (
 	<div
 		className={`fixed inset-0 -bottom-20 ${colorLight} ${colorDark} w-screen -z-10`}
