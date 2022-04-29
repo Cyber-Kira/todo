e85b4dc5924998d0990af543f73db00434da520f
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-04-29 17:26:39 +0300

    feat: update task button to show overlay when pressed

--- src/components/AddTaskButton/AddTaskButton.tsx
+++ src/components/AddTaskButton/AddTaskButton.tsx
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react'
 import AddRoundedIcon from '@mui/icons-material/AddRounded'
 import { AddItemContext } from '../App'
+import { AddTaskOverlay } from '../AddTaskOverlay'
 
 export const AddTaskButton = () => {
 	const AddTaskCtx = useContext(AddItemContext)
-	const expandOverlay = () => {
+	const toggleOverlay = () => {
 		AddTaskCtx!.setOverlay(!AddTaskCtx!.overlay)
 	}
 
@@ -12,20 +13,23 @@ export const AddTaskButton = () => {
 		<div
 			className={`fixed w-[2000px] h-[2000px] rounded-full bg-white ${
 				AddTaskCtx!.overlay ? 'scale-100' : 'scale-0'
-			} transition-all duration-300`}
+			} transition-all duration-200 z-50`}
 		/>
 	)
 
 	return (
-		<button
-			type='button'
-			onClick={expandOverlay}
-			className={`flex items-center justify-center fixed bottom-8 right-8 z-50 w-16 h-16 rounded-full shadow-lg bg-menuItemLight text-white ${
-				AddTaskCtx!.overlay ? 'pointer-events-none' : ''
-			}`}
-		>
-			<AddRoundedIcon fontSize='large' />
-			{overlayBackground}
-		</button>
+		<>
+			<button
+				type='button'
+				onClick={toggleOverlay}
+				className={`flex items-center justify-center fixed bottom-8 right-8 z-20 w-16 h-16 rounded-full shadow-lg bg-menuItemLight text-white ${
+					AddTaskCtx!.overlay ? 'pointer-events-none' : ''
+				}`}
+			>
+				<AddRoundedIcon fontSize='large' />
+				{overlayBackground}
+			</button>
+			<AddTaskOverlay />
+		</>
 	)
 }
