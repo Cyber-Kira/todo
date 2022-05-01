7cac827bad4c6165ce6a4611966e8b0aefe3af20
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-01 19:30:07 +0300

    refactor: separate components

---
+++ src/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined'
+import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded'
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined'
+import { ThemeToggle } from '../ThemeToggle'
+import image from '../../avatar.png'
+
+interface Props {
+	open: boolean
+	toggleMenu: () => void
+}
+
+export const SideMenu = ({ open, toggleMenu }: Props) => (
+	<div
+		className={`fixed -translate-x-[calc(100%+300px)] top-5 left-0 bottom-0 flex flex-col gap-5 w-3/5 ${
+			open ? 'translate-x-[calc(0px)]' : 'duration-500'
+		} transition ease-in-out duration-[650ms]`}
+	>
+		<button
+			onClick={toggleMenu}
+			className='absolute flex justify-center items-center right-0 w-12 h-12 rounded-full border-2 border-slate-500 border-opacity-25 text-light'
+			type='button'
+		>
+			<ChevronLeftRoundedIcon fontSize='large' />
+		</button>
+		<div className='flex flex-col gap-12 ml-9'>
+			<button className='relative w-24 h-24 mt-12 rounded-full' type='button'>
+				<img
+					className='rounded-full object-cover w-full h-full'
+					src={image}
+					alt='text'
+				/>
+			</button>
+			<p className='text-white text-4xl font-mono'>Kirill Koloskov</p>
+			<ul>
+				<li className='flex items-center'>
+					<div className='inline-block w-6 h-6 mb-1 text-slate-500 border-opa'>
+						<BookmarkBorderOutlinedIcon />
+					</div>
+					{/* Will become router link when i finish with components */}
+					<button
+						type='button'
+						onClick={toggleMenu}
+						className='inline-block text-md font-mono text-lightDimmed ml-1 py-3 pl-4 pr-20'
+					>
+						Categories
+					</button>
+				</li>
+				<li className='flex items-center'>
+					<div className='inline-block w-6 h-6 mb-1 text-slate-500 border-opa'>
+						<AppsOutlinedIcon />
+					</div>
+					{/* Will become router link when i finish with components */}
+					<button
+						type='button'
+						onClick={toggleMenu}
+						className='inline-block text-md font-mono text-lightDimmed ml-1 py-3 pl-4 pr-20'
+					>
+						Analytics
+					</button>
+				</li>
+			</ul>
+		</div>
+		<div className='mt-auto mb-8'>
+			<ThemeToggle />
+		</div>
+	</div>
+)
