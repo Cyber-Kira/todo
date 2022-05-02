24a1d2f9b22fdd44f0cbb06802626d6c6c0cd4ba
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-02 19:04:56 +0300

    feat: add function to add new items to list

--- src/components/AddTaskOverlay/AddTaskOverlay.tsx
+++ src/components/AddTaskOverlay/AddTaskOverlay.tsx
@@ -1,11 +1,32 @@
 /* eslint-disable arrow-body-style */
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import CloseIcon from '@mui/icons-material/Close'
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
-import { AddItemContext } from '../App'
+import { AddItemContext, TodoItemInterface } from '../App'
+import { generateRandomId } from '../../utils'
 
 export const AddTaskOverlay = () => {
 	const AddTaskCtx = useContext(AddItemContext)
+	const [value, setValue] = useState<TodoItemInterface>({
+		id: '',
+		title: '',
+	})
+
+	const setTodo = () => {
+		if (value !== undefined) {
+			AddTaskCtx?.setTodoCollection(prevState => [...prevState, value])
+		}
+		setValue({ id: '', title: '' })
+		AddTaskCtx?.setOverlay(false)
+	}
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setValue({
+			id: generateRandomId(),
+			title: e.target.value,
+		})
+	}
+
 	return (
 		<div
 			className={`flex flex-col justify-between px-9 py-12 absolute top-0 left-0 z-50 w-screen h-[calc(100vh-50px)] pointer-events-none ${
@@ -26,6 +47,8 @@ export const AddTaskOverlay = () => {
 				</button>
 			</div>
 			<input
+				onChange={e => handleChange(e)}
+				value={value.title}
 				className={`bg-transparent h-12 indent-2 ${
 					AddTaskCtx?.overlay ? 'translate-y-0' : '-translate-y-4'
 				} transition-transform delay-150 duration-700`}
@@ -34,6 +57,7 @@ export const AddTaskOverlay = () => {
 				placeholder='Enter new task'
 			/>
 			<button
+				onClick={setTodo}
 				className={`flex gap-1 justify-center items-center bg-menuItemLight w-1/2 h-12 self-end rounded-full text-white ${
 					AddTaskCtx?.overlay
 						? 'translate-y-0 opacity-100'
