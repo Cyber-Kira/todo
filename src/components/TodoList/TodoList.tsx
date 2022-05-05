a87782fbb8b4e201e725c036a0b47e89b642bf8f
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-05 17:26:54 +0300

    feat: add filter for todo list

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,22 +1,87 @@
 /* eslint-disable arrow-body-style */
-import React from 'react'
+import React, { useContext } from 'react'
+import { AddItemContext } from '../App'
 import { TotoItem } from '../TodoItem'
 
 interface TodoItemInterface {
 	id: string
 	title: string
+	isCompleted: boolean
+	category: string
 }
 
-interface TodoListInterface {
+export const TodoList = ({
+	filterString,
+	todoItems,
+}: {
+	filterString: string
 	todoItems: TodoItemInterface[]
-}
-
-export const TodoList = ({ todoItems }: TodoListInterface) => {
+}) => {
+	const AddTaskCtx = useContext(AddItemContext)
+	if (filterString === 'active') {
+		return (
+			<div className='flex flex-col gap-1 h-full mb-8'>
+				{todoItems
+					.filter(item => item.isCompleted === false)
+					.map(todoItem => {
+						return (
+							<TotoItem
+								key={todoItem.id}
+								id={todoItem.id}
+								title={todoItem.title}
+								isCompleted={todoItem.isCompleted}
+							/>
+						)
+					})}
+			</div>
+		)
+	}
+	if (filterString === 'completed') {
+		return (
+			<div className='flex flex-col gap-1 h-full mb-8'>
+				<button
+					onClick={() => {
+						AddTaskCtx?.setTodoCollection(prevState => {
+							const newTodoCollection = prevState.filter(
+								todoItem => todoItem.isCompleted !== true
+							)
+							window.localStorage.setItem(
+								'todoCollection',
+								JSON.stringify(newTodoCollection)
+							)
+							return newTodoCollection
+						})
+					}}
+					className='bg-red-600 dark:text-white p-2 rounded-lg'
+					type='button'
+				>
+					Delete Everything
+				</button>
+				{todoItems
+					.filter(item => item.isCompleted === true)
+					.map(todoItem => {
+						return (
+							<TotoItem
+								key={todoItem.id}
+								id={todoItem.id}
+								title={todoItem.title}
+								isCompleted={todoItem.isCompleted}
+							/>
+						)
+					})}
+			</div>
+		)
+	}
 	return (
 		<div className='flex flex-col gap-1 h-full mb-8'>
 			{todoItems.map(todoItem => {
 				return (
-					<TotoItem key={todoItem.id} id={todoItem.id} title={todoItem.title} />
+					<TotoItem
+						key={todoItem.id}
+						id={todoItem.id}
+						title={todoItem.title}
+						isCompleted={todoItem.isCompleted}
+					/>
 				)
 			})}
 		</div>
